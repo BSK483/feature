dd0d65a41478fbd5312821ac9674e2865443a79a
saikrishna <krishnasai@gmail.com>
2026-03-29 04:17:04 +0000

    2nd code is ready for testing

--- krishna.py
+++ krishna.py
@@ -2,4 +2,13 @@
 
 print("Hello, World!")
 
+/////// CODE 2 Is ready //////////
 
+# This program adds two numbers
+# Input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+# Add two numbers
+sum = num1 + num2
+# Display the sum
+print("The sum is:", sum)
